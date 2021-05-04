43fc2cef998a405a4998ece167d77c95523c056f
Rob Gordon <rchristopher.gordon@gmail.com>
2021-05-04 16:52:29 -0400

    feat(generate): add generate files

---
+++ components/src/generate.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+import path from "path";
+import { RegisterOptions } from "ts-node";
+
+const tsNodeRegisterOptions: RegisterOptions = {
+  transpileOnly: true,
+  compilerOptions: {
+    module: "commonjs",
+    target: "es6",
+  },
+};
+
+require("ts-node").register(tsNodeRegisterOptions);
+
+const themeIndex = process.argv[2];
+const style = process.argv[3] ?? "./style.css";
+if (!themeIndex)
+  throw new Error("Argument 0 should be the path to the root of your theme");
+
+const themeIndexPath = path.resolve(process.cwd(), themeIndex);
+
+function generateTheme() {
+  // required to get new versions when watching
+  delete require.cache[require.resolve(themeIndexPath)];
+  const { css } = require(themeIndexPath);
+  if (!css)
+    throw new Error(
+      `css export missing from ${themeIndex}\n\nMake sure to add:\n\nexport { css } from "@tone-row/components";\n\nat the end of your file`
+    );
+
+  const stylePath = path.resolve(process.cwd(), style);
+  fs.writeFileSync(stylePath, css(), "utf-8");
+  console.log(`Generated ${style}`);
+}
+
+const watch = process.argv.slice(2)[1];
+
+if (watch === "-w") {
+  generateTheme();
+  console.log("Watching config for changes 👀");
+  fs.watch(themeIndexPath, { encoding: "utf-8" }, () => {
+    try {
+      generateTheme();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+} else {
+  generateTheme();
+}
